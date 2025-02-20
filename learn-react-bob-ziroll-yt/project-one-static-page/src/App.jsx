f8ad7f44bc6b00e7809b01c2767484ffda748676
nicky-mc <cinimod@gmail.com>
2025-02-20 14:33:17 +0000

    projet set up

--- learn-react-bob-ziroll-yt/project-one-static-page/src/App.jsx
+++ learn-react-bob-ziroll-yt/project-one-static-page/src/App.jsx
@@ -1,4 +1,15 @@
 import { useState } from 'react'
-
+import MainContent from './components/MainContent.jsx';
 import './App.css'
 
+export default function App () {
+  return (
+    <div className='app'>
+      <header>
+        <h1>Hello but with React</h1>
+        <div className='underline'></div>
+      </header>
+      <MainContent />
+    </div>
+  )
+}
---
+++ learn-react-bob-ziroll-yt/project-one-static-page/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainContent () {
+    return (
+        <main>
+            <h2>This is the main content</h2>
+        </main>
+    );
+}
